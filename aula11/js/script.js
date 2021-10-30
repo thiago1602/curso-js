baefc1b3d6254fdcf5686ad189d4dfeef49e22a1
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-10-29 22:40:31 -0300

    inicio

---
+++ aula11/js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro numero');
+
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${num1 + num2}` );
---
+++ aula13/aula13.js
@@ -0,0 +1,7 @@
+let umaString =  "O rato roeu a roupa do rei de roma.";
+
+//tamanho da string
+console.log(umaString.length);
+
+//start na palavra ex. rato do
+console.log(umaString.slice(2, 6));
