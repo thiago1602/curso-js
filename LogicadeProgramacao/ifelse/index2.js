9cc6100edb7bb43a6d20beb85138b24e15eee7b4
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-11-19 11:05:02 -0300

    mais

---
+++ LogicadeProgramacao/ifelse/index2.js
@@ -0,0 +1,14 @@
+const numero = 6;
+
+if(numero >= 0 && numero <= 5){
+    console.log('Sim, o número esta entre 0 e 5');
+}else if(numero >=6 && numero <= 8){
+    console.log('O numero esta entre 6 e 8');
+
+}else if (numero >= 9 && numero <=11){
+    console.log('O numero esta entre 9 e 11');
+
+}
+else {
+    console.log('O numero não esta entre 0 e 11')
+}
