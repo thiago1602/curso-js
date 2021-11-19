a134a5c3e3023f76750711011224f14c1bcceb66
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-11-18 22:04:45 -0300

    update

---
+++ LogicadeProgramacao/short-circuit/index.js
@@ -0,0 +1,15 @@
+/* function falaOi(){
+    return 'Oi';
+}
+
+let vaiExecutar = 'Joao';
+
+console.log(vaiExecutar && falaOi());
+*/
+
+console.log(0 || false || null || 'Thiago de Moraes' || true);
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);
