505dca4594acbd1c7d12f7c8781c18997eee9fbc
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-11-18 21:49:28 -0300

    mais mais

---
+++ LogicadeProgramacao/operadoreslogicos/index.js
@@ -0,0 +1,15 @@
+/*  && -> AND -> E todas a expressões precisam ser verdadeiras para retonar true
+ || -> OR -> OU 
+ ! -> NOT -> */
+
+ /* console.log(true && true);
+ console.log(true || true); */
+
+/*  const usuario = 'Thiago';
+ const senha = '123456';
+
+ const vaiLogar = usuario ==='Thiago' && senha === '12356';
+ console.log(vaiLogar);
+ */
+
+ console.log(!true);
